3559ee0bcf1554a32d7832e7c4e4df9fedc42e7f
nazifbara <nazifbarassounon@gmail.com>
2023-04-15 11:54:04 +0100

    Refactor the typography component

--- src/lib/components/Typography.svelte
+++ src/lib/components/Typography.svelte
@@ -2,41 +2,18 @@
 	export let el = 'p';
 	export let variant = 'body1';
 
-	let headingBase = 'font-bold';
-
-	let variants: { [any: string]: string } = {
-		h1: `
-      ${headingBase}
-      text-xl
-      md:leading-[1.88]
-      md:text-[2rem]
-    `,
-		h2: `
-      ${headingBase}
-      text-xl
-      leading-[1.9]
-    `,
-		h3: `
-      ${headingBase}
-      text-base
-      leading-[1.67]
-    `,
-		h4: `
-      ${headingBase}
-      text-xs
-      leading-tight
-    `,
-		body1: `
-      text-xs
-      leading-tight
-    `,
-		body2: `
-      text-[0.688rem]
-      leading-[1.64]
-    `
+	const variants: { [key: string]: string[] } = {
+		h1: ['text-xl', 'md:leading-[1.88]', 'md:text-[2rem]', 'font-bold'],
+		h2: ['text-xl', 'leading-[1.9]', 'font-bold'],
+		h3: ['text-base', 'leading-[1.67]', 'font-bold'],
+		h4: ['text-xs', 'leading-tight', 'font-bold'],
+		body1: ['text-xs', 'leading-tight'],
+		body2: ['text-[0.688rem]', 'leading-[1.64]']
 	};
+
+	const classNames = variants[variant].join(' ');
 </script>
 
-<svelte:element this={el} class={`${variants[variant]}`}>
+<svelte:element this={el} class={classNames}>
 	<slot />
 </svelte:element>
