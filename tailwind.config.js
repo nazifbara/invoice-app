35465e6b69282a05c089c9ebf8f3b14fe7e45563
nazifbara <nazifbarassounon@gmail.com>
2023-04-15 11:47:13 +0100

    add some custom colors to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ export default {
 		fontFamily: {
 			base: ['Spartan', 'sans-serif']
 		},
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#7C5DFA',
+				primary2: '#9277FF',
+				darkText: '#0C0E16',
+				darkText2: '#7E88C3',
+				darkBg: '#141625',
+				darkBg2: '#1E2139',
+				lightBg: '#F8F8FB',
+				lightBg2: '#FFF',
+				lightText: '#FFF',
+				lightText2: '#DFE3FA',
+				danger: '#EC5757',
+				danger2: '#FF9797',
+				success: '#33D69F',
+				success2: 'rgba(51, 214, 159, 0.571)',
+				warning: '#FF8F00',
+				warning2: 'rgba(255, 143, 0, 0.571)',
+				draft: '#373B53',
+				draft2: 'rgba(55, 59, 83, 0.571)'
+			}
+		}
 	},
 	plugins: []
 };
