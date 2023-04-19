42f2894424101b198afc09574763e38e865b5777
nazifbara <nazifbarassounon@gmail.com>
2023-04-19 10:44:09 +0100

    create the TextField component

---
+++ src/lib/components/TextField.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let label = '';
+</script>
+
+<label class="flex flex-col gap-3">
+	{#if label}
+		<span class="text-darkText2 dark:text-lightText2">{label}</span>
+	{/if}
+	<input
+		type="text"
+		class="bg-white text-darkText font-bold border border-lightText2 px-5 py-4 outline-none rounded-[0.25rem] focus:border-primary dark:bg-darkBg2 dark:border-[#252945] dark:text-white caret-primary"
+	/>
+</label>
---
+++ src/routes/input/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import TextField from '$lib/components/TextField.svelte';
+</script>
+
+<div class="grid gap-3 p-10 justify-start">
+	<TextField label="Street Address" />
+</div>
+
+<div class="grid gap-3 p-10 justify-start bg-[#141625] dark">
+	<TextField label="Street Address" />
+</div>
