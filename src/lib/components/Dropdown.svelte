f12aa3168b1aaa5ea9d536bf272cd529b6ad9ec6
nazifbara <nazifbarassounon@gmail.com>
2023-05-04 10:21:38 +0100

    add the Dropdown component

---
+++ src/lib/components/Dropdown.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	export let options: { value: string; label: string }[] = [];
+	export let label: string = '';
+	export let name: string = '';
+</script>
+
+<label class="flex flex-col gap-3">
+	{#if label}
+		<span class="text-darkText2 dark:text-lightText2">{label}</span>
+	{/if}
+	<div
+		class={`
+		w-full
+		relative
+		overflow-hidden
+		after:block
+		after:w-2
+		after:h-2
+		after:border-l-[0px]
+		after:border-l-transparent
+		after:border-b-[0px]
+		after:border-b-transparent
+		after:border-r-[2px]
+		after:border-r-primary
+		after:border-t-[2px]
+		after:border-t-primary
+		after:absolute
+		after:top-[50%]
+		after:-translate-y-1/2
+		after:rotate-[135deg]
+		after:right-5
+		after:content-[""]
+	`}
+	>
+		<select
+			{name}
+			class={`
+			appearance-none
+			bg-white
+			text-darkText
+			font-bold
+			border
+			border-lightText2
+			px-5
+			py-4
+			outline-none
+			rounded-[0.25rem]
+			w-full
+			z-50
+			focus:border-primary
+			dark:text-white
+			dark:bg-darkBg2
+			dark:border-[#252945]
+		`}
+		>
+			{#each options as option}
+				<option value={option.value}>
+					{option.label}
+				</option>
+			{/each}
+		</select>
+	</div>
+</label>
--- src/routes/input/+page.svelte
+++ src/routes/input/+page.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import TextField from '$lib/components/TextField.svelte';
+	import Dropdown from '$lib/components/Dropdown.svelte';
+
+	const options = [
+		{ value: '1', label: 'Net 1 Day' },
+		{ value: '7', label: 'Net 7 Days' },
+		{ value: '14', label: 'Net 14 Days' },
+		{ value: '30', label: 'Net 30 Days' }
+	];
 </script>
 
-<div class="grid gap-3 p-10 justify-start">
+<div class="grid gap-10 p-10 justify-start">
+	<Dropdown label="Payment Terms" {options} />
 	<TextField label="Street Address" />
 </div>
 
 <div class="grid gap-3 p-10 justify-start bg-[#141625] dark">
+	<Dropdown label="Payment Terms" {options} />
 	<TextField label="Street Address" />
 </div>
