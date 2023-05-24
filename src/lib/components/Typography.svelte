b2fd162bd261acb551b92d1316dbb4006e55e761
nazifbara <nazifbarassounon@gmail.com>
2023-05-24 16:46:29 +0100

    refactoring

--- src/lib/components/Typography.svelte
+++ src/lib/components/Typography.svelte
@@ -5,18 +5,24 @@
 	export let bold = false;
 
 	const variants: { [key: string]: string[] } = {
-		h1: ['text-xl', 'md:leading-[1.88]', 'md:text-[2rem]', 'font-bold', 'dark:text-lightText'],
-		h2: ['text-xl', 'leading-[1.9]', 'font-bold', 'dark:text-lightText'],
-		h3: ['text-base', 'leading-[1.67]', 'font-bold', 'dark:text-lightText'],
-		h4: ['text-xs', 'leading-tight', 'font-bold', 'dark:text-lightText'],
+		h1: ['text-xl', 'md:leading-[1.88]', 'md:text-[2rem]', 'dark:text-lightText'],
+		h2: ['text-xl', 'leading-[1.9]', 'dark:text-lightText'],
+		h3: ['text-base', 'leading-[1.67]', 'dark:text-lightText'],
+		h4: ['text-xs', 'leading-tight', 'dark:text-lightText'],
 		body1: ['text-xs', 'leading-tight', 'text-darkText3', 'dark:text-lightText'],
 		body2: ['text-xs', 'leading-tight', 'text-darkText2', 'dark:text-lightText2'],
-		body3: ['text-[0.688rem]', 'leading-[1.64]', 'text-darkText3', 'dark:text-lightText']
+		body3: ['text-[0.688rem]', 'leading-[1.64]', 'text-darkText3', 'dark:text-lightText'],
+		body4: ['text-xs', 'leading-tight', 'text-darkText', 'dark:text-white']
 	};
 
 	const classNames = variants[variant].join(' ');
 </script>
 
-<svelte:element this={as} class="{classNames} " class:inline class:font-bold={bold}>
+<svelte:element
+	this={as}
+	class={classNames}
+	class:font-bold={bold || variant.includes('h')}
+	class:inline
+>
 	<slot />
 </svelte:element>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,14 @@
 	<div class="flex items-center justify-between">
 		<div>
 			<Typography as="h1" variant="h1">Invoices</Typography>
-			<Typography as="p" variant="body2">
+			<Typography as="p" variant="body1">
 				<span class="hidden md:inline">There are {data.length} total invoices</span>
 				<span class="md:hidden">{data.length} invoices</span>
 			</Typography>
 		</div>
-		<div class="flex gap-[1.125rem]">
+		<div class="flex gap-[1.125rem] md:gap-10">
 			<button class="flex items-center gap-3 md:gap-4">
-				<Typography as="span" variant="body1" bold>
+				<Typography as="span" variant="body4" bold>
 					<span class="hidden md:inline">Filter by status</span>
 					<span class="md:hidden">Filter</span>
 				</Typography>
